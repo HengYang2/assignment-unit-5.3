c93139aad9463d12c519f08df93f3f8a082c4ce1
Heng Yang <hengya03@gmail.com>
2023-03-21 17:11:38 -0500

    Updated the addToCollection() to take in a 'tracks' parameter.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,24 +1,36 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
-function addToCollection( title, artist, yearPublished) {
+function addToCollection( title, artist, yearPublished, tracks) {
     let newObject = {
         title: title,
         artist: artist,
         yearPublished: yearPublished,
+        tracks: tracks,
     }
     collection.push(newObject);
     return newObject;
 }
 
+let song1 = {
+    trackName: 'Bush',
+    duration: '2:00',
+};
+let song2 = {
+    trackName: 'Apples',
+    duration: '1:30',
+};
+let reusableTracksArray = [song1, song2];
+
 //Testing addToCollection() function:
-console.log("Testing addToCollection():", addToCollection( 'Green Trees', 'Magical Elves', '1990'));
-console.log("Testing addToCollection():", addToCollection( 'Yellow Bells', 'Presents For You', '1996'));
-console.log("Testing addToCollection():", addToCollection( 'Blue Clouds', 'Skyscrapers', '2010'));
-console.log("Testing addToCollection():", addToCollection( 'Red Boots', 'Jaguars', '1989'));
-console.log("Testing addToCollection():", addToCollection( 'Silver Night', 'Still In School', '1996'));
-console.log("Testing addToCollection():", addToCollection( 'Brown Dirt', 'Magical Elves', '2011'));
+console.log("Testing addToCollection():", addToCollection( 'Green Trees', 'Magical Elves', '1990', reusableTracksArray));
+console.log("Testing addToCollection():", addToCollection( 'Yellow Bells', 'Presents For You', '1996', reusableTracksArray));
+console.log("Testing addToCollection():", addToCollection( 'Blue Clouds', 'Skyscrapers', '2010', reusableTracksArray));
+console.log("Testing addToCollection():", addToCollection( 'Red Boots', 'Jaguars', '1989', reusableTracksArray));
+console.log("Testing addToCollection():", addToCollection( 'Silver Night', 'Still In School', '1996', reusableTracksArray));
+console.log("Testing addToCollection():", addToCollection( 'Brown Dirt', 'Magical Elves', '2011', reusableTracksArray));
 console.log('Checking updated array:', collection);
+
 
 function showCollection( collection ) {
     console.log('Total number of items in array:', collection.length);
@@ -27,6 +39,7 @@ function showCollection( collection ) {
     }
 }
 showCollection(collection);
+
 
 function findByArtist( artist ) {
     let array = [];
@@ -46,13 +59,13 @@ console.log('findByArtist() Results:', findByArtist('Jaguars'));
 console.log('findByArtist() Results:', findByArtist('Magical Elves')); 
 
 //Stretch Goal
-function search( artist, yearPublished) {
+function search( artist, yearPublished, trackName) {
     let array = [];
-    if (artist == undefined || yearPublished == undefined) {
+    if (artist == undefined || yearPublished == undefined || trackName == undefined) {
         return collection;
     }
     for (const album of collection) {
-        if (album.artist === artist && album.yearPublished === yearPublished) {
+        if (album.artist === artist && album.yearPublished === yearPublished && album.trackName == trackName) {
             array.push(album);
         }
     }
@@ -60,6 +73,7 @@ function search( artist, yearPublished) {
 }
 
 //Testing
-console.log('search() - expect array with 1 object:', search('Magical Elves', '2011'));
+console.log('search() - expect array with 1 object:', search('Magical Elves', '2011', ''));
 console.log('search() - expect empty array:', search('Fantasy Gnomes', '2011'));
 console.log('search() - no arguments given, should expect whole collection array:', search());
+
