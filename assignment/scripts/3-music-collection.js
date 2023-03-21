447d2642ff1fcefe7942ced2d695e631075c73a8
Heng Yang <hengya03@gmail.com>
2023-03-21 15:29:57 -0500

    Editted addToCollection(), made it so a new object was created with input parameters, added to the 'collection' array and returns the new object created.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,7 +2,14 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection( title, artist, yearPublished) {
-
-
-
+    let newObject = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(newObject);
+    return newObject;
 }
+
+console.log("testing addToCollection():", addToCollection( 'Best-Song', 'Unknown', '1987'));
+console.log('checking updated array:', collection);
